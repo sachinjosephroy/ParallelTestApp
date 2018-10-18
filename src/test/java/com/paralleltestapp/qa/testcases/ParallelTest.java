f4f4da213642454849df683cc54836eecbc72aa2
Sachin Roy <sachin.jr@gmail.com>
2018-10-17 23:28:19 -0400

    Initial commit

--- src/test/java/com/paralleltestapp/qa/testcases/ParallelTest.java
+++ src/test/java/com/paralleltestapp/qa/testcases/ParallelTest.java
@@ -10,7 +10,7 @@ public class ParallelTest extends Testbase {
 	@Test
 	public void testTitle() {
 		String actual = login.getTitle();
-		String expected = "#1 Free CRM software in the cloud for sales and service";
+		String expected = "#1 Free CRM software in the cloud for sales and service.";
 		Assert.assertEquals(actual, expected);
 	}
 
