19eec4eb7f5ad3ef3d6e6f21bd5d802180f7e1ca
Sachin Roy <sachin.jr@gmail.com>
2018-10-17 21:54:54 -0400

    Initial commit

--- src/main/java/com/paralleltestapp/qa/base/Testbase.java
+++ src/main/java/com/paralleltestapp/qa/base/Testbase.java
@@ -32,25 +32,11 @@ public class Testbase {
 				System.setProperty("webdriver.chrome.driver", "C:\\Selenium Grid\\chromedriver.exe");
 				driver = new ChromeDriver();
 			}
-			else if(myBrowser.equalsIgnoreCase("firefox")) {
-				System.setProperty("webdriver.gecko.driver", "C:\\Selenium Grid\\geckodriver.exe");
-				driver = new FirefoxDriver();
-			}
-			else if(myBrowser.equalsIgnoreCase("edge")) {
-				System.setProperty("webdriver.edge.driver", "C:\\Selenium Grid\\MicrosoftWebDriver.exe");
-				driver = new EdgeDriver();
-			}
 		}
 		else if(System.getProperty("os.name").equals("Windows 7")) {
 			if(myBrowser.equalsIgnoreCase("chrome")) {
 				System.setProperty("webdriver.chrome.driver", "C:\\Selenium Grid\\chromedriver.exe");
 				driver = new ChromeDriver();
-			}
-			else if(myBrowser.equalsIgnoreCase("firefox")) {
-				System.out.println("Windows 7 system does not have firefox browser installed on it");
-			}
-			else if(myBrowser.equalsIgnoreCase("edge")) {
-				System.out.println("Windows 7 system does not have edge browser installed on it");
 			}
 		}
 		driver.get("http://freecrm.com/index.html");
